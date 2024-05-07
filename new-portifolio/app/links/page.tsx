981211c7972f1967c276a1e85a29549a2c4a0c56
guilhermegattimarinho <ggattimarinho@gmail.com>
2024-05-07 02:26:01 -0300

    feat: add sample links

--- new-portifolio/app/links/page.tsx
+++ new-portifolio/app/links/page.tsx
@@ -1,10 +1,48 @@
+import Link from "next/link";
+import { BsGithub } from "react-icons/bs";
 import MainLayout from "../components/MainLayout";
 
 export default function Links() {
   return (
     <MainLayout>
-      <h1>Links</h1>
-      <p>Esta é a página de links</p>
+      <section className="mt-32 grid grid-cols-3 grid-rows-3 gap-x-1 gap-y-12 justify-items-center">
+        <Link href='https://github.com/guido-marinho' target="_blank">
+          <BsGithub 
+            size={50}
+            color=" rgba(255, 255, 255, 0.2)"
+          />
+        </Link>
+              <Link href='https://github.com/guido-marinho' target="_blank">
+          <BsGithub 
+            size={50}
+            color=" rgba(255, 255, 255, 0.2)"
+          />
+        </Link>
+              <Link href='https://github.com/guido-marinho' target="_blank">
+          <BsGithub 
+            size={50}
+            color=" rgba(255, 255, 255, 0.2)"
+          />
+        </Link>
+              <Link href='https://github.com/guido-marinho' target="_blank">
+          <BsGithub 
+            size={50}
+            color=" rgba(255, 255, 255, 0.2)"
+          />
+        </Link>
+              <Link href='https://github.com/guido-marinho' target="_blank">
+          <BsGithub 
+              size={50}
+              color=" rgba(255, 255, 255, 0.2)"
+          />
+        </Link>
+              <Link href='https://github.com/guido-marinho' target="_blank">
+          <BsGithub 
+              size={50}
+              color=" rgba(255, 255, 255, 0.2)"
+          />
+        </Link>
+      </section>
     </MainLayout>
   )
 }
